8694569ab27dccfbb10ca8289a66623be903523b
Hanna <hsoonhanna0@gmail.com>
2024-07-16 08:16:31 +0200

    job seeker schema

---
+++ back-end/models/jobSeekerModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const jobSeekerSchama = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      resume: { type: String },
+      coverLetter: { type: String },
+      profilePic: { type: String },
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("JobSeeker", jobSeekerSchama);
