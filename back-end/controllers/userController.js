26e5a25be6f5f4b296f8f5920bf6d622303916fe
akr771 <akr772129462@gmail.com>
2024-07-16 12:04:26 +0200

    add logIn by the type and add validator to SignUp

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,10 +1,20 @@
 const employee = require("../models/Schema")
-
-
+const company= require('../models/')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const { validationResult } = require("express-validator");
 
 const  SignUp = (req, res) => {
     console.log('receive  call  of the api');
     console.log(req.body);
+    const errors = validationResult(req);
+    const errorMessages = errors.array();
+    if (!errors.isEmpty()) {
+        console.log(errorMessages);
+        return res.status(400).json({
+           SignUpError: errorMessages ,
+          });
+        }else{
     //create an employee
     var  employeeObj = {
         firstName: "Abd",
@@ -14,9 +24,56 @@ const  SignUp = (req, res) => {
     const data =employee (employeeObj)
     //data.save();
     res.send(employeeObj)
-}
+}}
 
-
+const loginUser = async (req, res) => {
+    try {
+      const type = req.params.type;
+      let typeClient;
+  
+      if (type === "employee") {
+        typeClient = employee;
+      } else {
+        typeClient = company;
+      }
+  
+      let existUser = await typeClient.findOne({ email: req.body.email });
+      if (!existUser) {
+        return res.status(400).json({
+          loginError: "Email does not exist, please register first.",
+        });
+      }
+  
+      let correctPassword = bcrypt.compareSync(req.body.password, existUser.password);
+      if (!correctPassword) {
+        return res.status(400).json({
+          loginError: "Password is not correct",
+        });
+      }
+  
+      // Create user token
+      let userDataForToken = {
+        id: existUser._id,
+        firstName: existUser.firstName,
+        lastName: existUser.lastName,
+        email: existUser.email,
+      };
+  
+      let userToken = jwt.sign({ user: userDataForToken }, "special");
+  
+      res.cookie("userToken", userToken);
+      res.status(200).json({
+        message: "Login successful",
+        token: userToken,
+      });
+    } catch (error) {
+      res.status(500).json({
+        loginError: "Internal server error",
+      });
+    }
+  };
+  
+  module.exports = loginUser;
 
 
 
@@ -24,4 +81,5 @@ const  SignUp = (req, res) => {
 
 module.exports = {
     SignUp,
+    loginUser
 }
