0cdd7a5214b73f77ce7d794058b52982ec98d051
Hanna <hsoonhanna0@gmail.com>
2024-07-16 16:42:54 +0200

    employer schema

---
+++ back-end/models/employerModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const employerSchama = new mongoose.Schema(
+  {
+    companyName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    aboutCompany: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Employer", employerSchama);
