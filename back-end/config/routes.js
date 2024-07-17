1e098af57179878c21a44e5d9dcbd8e2939725ef
Hanna <hsoonhanna0@gmail.com>
2024-07-17 11:40:08 +0200

    employer validator and the error massege on the front end

--- back-end/config/routes.js
+++ back-end/config/routes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const usercontroller = require("../controllers/userController");
 const registerValidator = require("../middleware/registerValidator");
+const employerValidator = require("../middleware/employerValidator");
 
 router.post("/", registerValidator, usercontroller.SignUp);
 router.post("/login/employee", usercontroller.loginUser);
-router.post("/register/employer", usercontroller.registerEmployer);
+router.post(
+  "/register/employer",
+  employerValidator,
+  usercontroller.registerEmployer
+);
 
 module.exports = router;
