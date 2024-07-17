b90d905df98e419a8c38cf25dd26a2619fde0abb
akr771 <akr772129462@gmail.com>
2024-07-17 14:37:24 +0200

    log In

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,33 @@
+import React, { useState } from 'react';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import LoginForm from "./components/LogIn";
 import Register from "./components/Register";
+import Cookies from 'js-cookie';
 
 function App() {
+  const [loggedInEmployee, setLoggedInEmployee] = useState(!!Cookies.get('token'));
+  const onLogin=(token)=>{
+    Cookies.set('token',token)
+    setLoggedInEmployee(true);
+  }
+
+  const handleLogout = () => {
+    Cookies.remove('token'); // Remove token from cookies
+    setLoggedInEmployee(false);
+  };
   return (
     <BrowserRouter>
       <div className="App">
+      <button onClick={handleLogout}>logOut</button>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
+          <Route path="/login" element={<LoginForm  onLogin={onLogin}/>} />
           <Route path="/register" element={<Register />} />
+          <Route path="/new" element={loggedInEmployee?<p>you are log in</p>:<p>you are not log in</p>} />
+
         </Routes>
       </div>
     </BrowserRouter>
--- front-end/src/components/LogIn.jsx
+++ front-end/src/components/LogIn.jsx
@@ -1,58 +1,126 @@
+
   import React, { useState } from 'react';
+  import { Avatar, Button, TextField, FormControlLabel, Grid, Box, Typography, Container, CssBaseline, Link, Radio, RadioGroup, FormControl, FormLabel } from '@mui/material';
+  import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
   import axios from '../axiosConfig';
-
-const LogIn = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setError('');
-
-    try {
-      const response = await axios.post('/login/employee', { email, password });
-
-      console.log("Login successful", response.data);
-      // Proceed with the login success (e.g., saving the token, redirecting the user, etc.)
-    } catch (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code that falls out of the range of 2xx
-        setError(error.response.data.loginError);
-      } else if (error.request) {
-        // The request was made but no response was received
-        setError('No response received');
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        setError(error.message);
+  import { createTheme, ThemeProvider } from '@mui/material/styles';
+  
+  const theme = createTheme();
+  
+  const LoginForm = ({onLogin}) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userType, setUserType] = useState('employee');
+    const [error, setError] = useState('');
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      setError('');
+      if (!email || !password) {
+          setError('Both email and password are required.');
+          return;
+        }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+          setError('Please enter a valid email address.');
+          return;
+        }
+      try {
+        const response = await axios.post(`http://localhost:5000/login/`, { email, password ,userType });
+        console.log("Login successful", response.data);
+        console.log(response.data.token);
+        onLogin(response.data.token)
+        // Handle login success (e.g., store token in cookies, redirect user, etc.)
+      } catch (error) {
+        if (error.response) {
+          setError(error.response.data.loginError);
+        } else if (error.request) {
+          setError('No response received');
+        } else {
+          setError(error.message);
+        }
       }
-    }
+    };
+  
+    return (
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+               <FormControl component="fieldset" sx={{ mt: 2 }}>
+                <FormLabel component="legend">User Type</FormLabel>
+                <RadioGroup
+                  row
+                  aria-label="userType"
+                  name="userType"
+                  value={userType}
+                  onChange={(e) => setUserType(e.target.value)}
+                >
+                  <FormControlLabel value="employee" control={<Radio />} label="employee" />
+                  <FormControlLabel value="employer" control={<Radio />} label="employer" />
+                </RadioGroup>
+              </FormControl>
+              {error && <Typography color="error" align="center">{error}</Typography>}
+              
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link href="/" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    );
   };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <button type="submit">Login</button>
-    </form>
-  );
-};
-
-export default LogIn;
+  
+  export default LoginForm;
+  
